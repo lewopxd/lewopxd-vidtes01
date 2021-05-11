09e176e63cc13acd24d18472466f341d1d8b1f5c
lewopxd <lewopxd.1@gmail.com>
2021-05-11 06:00:27 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hello!')
+
+
+ console.log('hello!')
+
+var options = {};
+
+ 
+
+
+var player = videojs('my-player', options, function onPlayerReady() {
+  videojs.log('Your player is ready!');
+
+  // In this context, `this` is the player that was created by Video.js.
+  this.play();
+
+  // How about an event listener?
+  this.on('ended', function() {
+    videojs.log('Awww...over so soon?!');     
+  });
+ this.on('play', function() {
+    videojs.log('Play');    
+  });
+
+ this.on('pause', function() {
+    videojs.log('Pause');    
+  });
+
+
+});
+
+
